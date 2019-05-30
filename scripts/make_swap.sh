8edb2b70c95c92b50ee15f9b7cbbe79dbc699d8c
Thanh Nguyen <sthanhng@gmail.com>
2019-05-30 09:01:51 +0700

    update and rename make_swapfile.sh

--- scripts/make_swap.sh
+++
@@ -1,7 +0,0 @@
-sudo fallocate -l 6G /mnt/swapfile
-sudo chmod 600 /mnt/swapfile
-sudo mkswap /mnt/swapfile
-sudo swapon /mnt/swapfile
-
-sudo cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
